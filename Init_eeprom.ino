15e982a708327f2b8de3e7001871a31d12e0aa6d
gullivigne <46405446+gullivigne@users.noreply.github.com>
2019-02-16 01:08:47 +0100

    update with eeprom put

--- Init_eeprom.ino
+++ Init_eeprom.ino
@@ -1,26 +1,54 @@
+/*
+ * EEPROM Clear
+ *
+ * Sets all of the bytes of the EEPROM to 0.
+ * Please see eeprom_iteration for a more in depth
+ * look at how to traverse the EEPROM.
+ *
+ * This example code is in the public domain.
+ */
+
 #include <EEPROM.h>
 int hi_address = 0;
-int hunger_address=2;
-int happiness_address=3;
-int health_address =4;
-int discipline_address=5;
-int weight_address=6;
-int age_address=7;
+int hunger_address=20;
+int happiness_address=30;
+int health_address =40;
+int discipline_address=50;
+int weight_address=60;
+int age_address=70;
 
 void setup() {
+  // initialize the LED pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+ 
+  /***
+    Iterate through each byte of the EEPROM storage.
+
+    Larger AVR processors have larger EEPROM sizes, E.g:
+    - Arduno Duemilanove: 512b EEPROM storage.
+    - Arduino Uno:        1kb EEPROM storage.
+    - Arduino Mega:       4kb EEPROM storage.
+
+    Rather than hard-coding the length, you should use the pre-provided length function.
+    This will make your code portable to all AVR processors.
+  ***/
 
   for (int i = 0 ; i < EEPROM.length() ; i++) {
     EEPROM.write(i, 0);
   }
+        float age = 0;
+        float hunger = 100;
+        float happiness = 100;
+        float discipline = 100;
+        float health = 100;
+        float weight = 1 ;
 
-
-  EEPROM.write(age_address, 0);
-  EEPROM.write(hunger_address, 100);
-  EEPROM.write(happiness_address, 100);
-  EEPROM.write(discipline_address, 100);
-  EEPROM.write(health_address, 100);
-  EEPROM.write(weight_address, 1);
+        EEPROM.put(age_address, age);
+        EEPROM.put(hunger_address, hunger);
+        EEPROM.put(happiness_address, happiness);
+        EEPROM.put(discipline_address, discipline);
+        EEPROM.put(health_address, health);
+        EEPROM.put(weight_address, weight);
 
 
   // turn the LED on when we're done
